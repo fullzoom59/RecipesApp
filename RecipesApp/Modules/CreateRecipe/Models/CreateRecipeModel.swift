e83ca7a7b8e10f19d32219bf4a37a6e439304541
Vlad <119012478+fullzoom59@users.noreply.github.com>
2023-10-09 17:08:51 +0200

    Delete RecipesApp/Modules/CreateRecipe/Models/CreateRecipeModel.swift

--- RecipesApp/Modules/CreateRecipe/Models/CreateRecipeModel.swift
+++
@@ -1,8 +0,0 @@
-import RealmSwift
-
-class RecipeModel: Object {
-    @Persisted var recipeImage: Data?
-    @Persisted var recipeName: String = "Unknown"
-    @Persisted var serves: String = "Unknown"
-    @Persisted var cookTime: String = "Unknown"
-}
